49688978abbd7b12764b211ff192425d6dcb188d
unknown <ushka_ya@mail.ru>
2024-10-26 19:45:42 +0300

    fixed: adding products to the shopping cart in mobile

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -9,14 +9,33 @@ const Cart: React.FC = () => {
   const dispatch = useDispatch();
   const items = useSelector((state: RootState) => state.products.cartItems);
 
-  const handleDrop = (event: React.DragEvent | React.TouchEvent) => {
+  const handleDrop = (event: React.DragEvent) => {
     event.preventDefault();
-    const product = (event as React.DragEvent).dataTransfer
-      ? (event as React.DragEvent).dataTransfer.getData('product')
-      : (event.target as HTMLElement).dataset.product;
-
+    const product = event.dataTransfer.getData('product');
     if (product) {
       dispatch(addToCart(product));
+    }
+  };
+
+  const handleTouchEnd = (event: React.TouchEvent) => {
+    const touch = event.changedTouches[0];
+    const cartElement = document.querySelector('.cart-container');
+
+    if (cartElement) {
+      const cartRect = cartElement.getBoundingClientRect();
+      if (
+        touch.clientX >= cartRect.left &&
+        touch.clientX <= cartRect.right &&
+        touch.clientY >= cartRect.top &&
+        touch.clientY <= cartRect.bottom
+      ) {
+        const draggedProduct = document
+          .querySelector('.product[draggable="true"]')
+          ?.getAttribute('data-src');
+        if (draggedProduct) {
+          dispatch(addToCart(draggedProduct));
+        }
+      }
     }
   };
 
@@ -25,12 +44,12 @@ const Cart: React.FC = () => {
       className="cart-container"
       onDrop={handleDrop}
       onDragOver={(e) => e.preventDefault()}
-      onTouchEnd={handleDrop}
+      onTouchEnd={handleTouchEnd} // Завершение тача
     >
       <img src={cart} alt="Корзина" className="cart-img" />
       <div className="cart-items">
         {items.map((item, index) => (
-          <div key={index} className="cart-item enlarged">
+          <div key={index} className="cart-item">
             <img src={item.src} alt="item in cart" />
           </div>
         ))}
--- src/components/Shelf.tsx
+++ src/components/Shelf.tsx
@@ -1,21 +1,85 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../store/productsSlice';
 import { RootState } from '../store/store';
 import '../assets/styles/Shelf.scss';
 
 const Shelf: React.FC = () => {
+  const dispatch = useDispatch();
   const { dairyProducts, meatProducts, fruitProducts } = useSelector(
     (state: RootState) => state.products
   );
 
-  const handleDragStart = (
-    event: React.DragEvent | React.TouchEvent,
-    productSrc: string
-  ) => {
-    if ('dataTransfer' in event) {
-      event.dataTransfer.setData('product', productSrc);
-    } else {
-      (event.target as HTMLElement).dataset.product = productSrc;
+  const [draggedProduct, setDraggedProduct] = useState<string | null>(null);
+  const [cloneElement, setCloneElement] = useState<HTMLDivElement | null>(null);
+
+  const handleDragStart = (event: React.DragEvent, productSrc: string) => {
+    event.dataTransfer.setData('product', productSrc);
+  };
+
+  const handleTouchStart = (event: React.TouchEvent, productSrc: string) => {
+    setDraggedProduct(productSrc);
+
+    const touch = event.touches[0];
+    const productElement = (event.target as HTMLElement).closest(
+      '.product'
+    ) as HTMLElement;
+    if (productElement) {
+      // Создаем клон элемента
+      const clone = productElement.cloneNode(true) as HTMLDivElement;
+      clone.style.position = 'fixed';
+      clone.style.pointerEvents = 'none';
+      clone.style.width = `${productElement.offsetWidth}px`;
+      clone.style.height = `${productElement.offsetHeight}px`;
+      clone.style.left = `${touch.clientX - productElement.offsetWidth / 2}px`;
+      clone.style.top = `${touch.clientY - productElement.offsetHeight / 2}px`;
+      clone.style.opacity = '0.8';
+      clone.style.zIndex = '1000';
+
+      document.body.appendChild(clone);
+      setCloneElement(clone);
+    }
+  };
+
+  const handleTouchMove = (event: React.TouchEvent) => {
+    const touch = event.touches[0];
+    const cartElement = document.querySelector('.cart-container');
+
+    if (cloneElement) {
+      // Обновляем позицию клона
+      cloneElement.style.left = `${touch.clientX - cloneElement.offsetWidth / 2}px`;
+      cloneElement.style.top = `${touch.clientY - cloneElement.offsetHeight / 2}px`;
+    }
+
+    if (cartElement) {
+      const cartRect = cartElement.getBoundingClientRect();
+      if (
+        touch.clientX >= cartRect.left &&
+        touch.clientX <= cartRect.right &&
+        touch.clientY >= cartRect.top &&
+        touch.clientY <= cartRect.bottom
+      ) {
+        cartElement.classList.add('hover');
+      } else {
+        cartElement.classList.remove('hover');
+      }
+    }
+  };
+
+  const handleTouchEnd = () => {
+    const cartElement = document.querySelector('.cart-container');
+    if (cartElement && cartElement.classList.contains('hover')) {
+      if (draggedProduct) {
+        dispatch(addToCart(draggedProduct));
+      }
+    }
+    setDraggedProduct(null);
+    cartElement?.classList.remove('hover');
+
+    // Удаляем клон
+    if (cloneElement) {
+      cloneElement.remove();
+      setCloneElement(null);
     }
   };
 
@@ -30,9 +94,9 @@ const Shelf: React.FC = () => {
         onDragStart={(event) =>
           !product.hidden && handleDragStart(event, product.src)
         }
-        onTouchStart={(event) =>
-          !product.hidden && handleDragStart(event, product.src)
-        }
+        onTouchStart={(event) => handleTouchStart(event, product.src)} // Начало тача
+        onTouchMove={handleTouchMove} // Перемещение тача
+        onTouchEnd={handleTouchEnd} // Завершение тача
         style={{ visibility: product.hidden ? 'hidden' : 'visible' }}
       >
         <img src={product.src} alt="product" />
